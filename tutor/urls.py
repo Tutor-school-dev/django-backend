4216e68bdc30adf868db5ab08b26fde1fea7d56d
Gaurav Bhinda <tutorschooldev@gmail.com>
2025-11-20 16:58:58 +0530

    dropped database

--- tutor/urls.py
+++ tutor/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', TutorDetailsView.as_view(), name='get-tutor-details'),
     path('create-account/', CreateTutorAccountView.as_view(), name='create-tutor-account'),
     path('add-details/', TutorDetailsView.as_view(), name='add-tutor-details'),
-    path('applied-jobs/', TutorJobDetails.as_view(), name-'get-tutor-applications')
+    path('applied-jobs/', TutorJobDetails.as_view(), name='get-tutor-applications')
 ]
